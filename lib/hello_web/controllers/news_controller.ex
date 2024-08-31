8a1a73c39811eac37c2ddc511a602bb153712dcc
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-08-31 18:24:35 +0700

    Change upload method

--- lib/hello_web/controllers/news_controller.ex
+++ lib/hello_web/controllers/news_controller.ex
@@ -1,8 +1,8 @@
-# lib/hello_web/controllers/news_controller.ex
 defmodule HelloWeb.NewsController do
   use HelloWeb, :controller
   alias Hello.News
   alias Hello.Repo
+  alias ExAws.S3
 
   plug HelloWeb.Plugs.AuthenticateUser when action in [:create]
 
@@ -11,26 +11,33 @@ defmodule HelloWeb.NewsController do
         "news_content" => news_content,
         "news_cover" => %Plug.Upload{} = upload
       }) do
-    # Get the current authenticated user from the connection
+    # Debug statements to check environment variables
+    IO.inspect(System.get_env("AWS_ACCESS_KEY_ID"), label: "AWS_ACCESS_KEY_ID")
+    IO.inspect(System.get_env("AWS_SECRET_ACCESS_KEY"), label: "AWS_SECRET_ACCESS_KEY")
+    IO.inspect(System.get_env("AWS_REGION"), label: "AWS_REGION")
+    IO.inspect(System.get_env("AWS_BUCKET_NAME"), label: "AWS_BUCKET_NAME")
+
     user = conn.assigns.current_user
 
-    # Construct the changeset with the authenticated user as the author
+    # Upload the file to S3 and get the URL
+    s3_url = upload_to_s3(upload)
+
     changeset =
       News.changeset(%News{}, %{
         "news_title" => news_title,
         "news_content" => news_content,
         # Default to draft status
         "news_status" => "draft",
-        "news_cover" => upload.filename,
+        "news_cover" => s3_url,
         "news_author_id" => user.id
       })
 
-    # Insert the new news record into the database
     case Repo.insert(changeset) do
-      {:ok, _news} ->
+      {:ok, news} ->
         conn
         |> put_status(:created)
-        |> json(%{message: "News created successfully."})
+        # Return the full news object
+        |> json(news)
 
       {:error, changeset} ->
         IO.inspect(changeset.errors, label: "Changeset errors")
@@ -47,5 +54,16 @@ defmodule HelloWeb.NewsController do
         String.replace(acc, "%{#{key}}", to_string(value))
       end)
     end)
+  end
+
+  defp upload_to_s3(%Plug.Upload{path: path, filename: filename}) do
+    bucket = System.get_env("AWS_BUCKET_NAME")
+    s3_key = "uploads/news_covers/#{filename}"
+
+    {:ok, _} =
+      S3.put_object(bucket, s3_key, File.read!(path))
+      |> ExAws.request()
+
+    "https://#{bucket}.s3.#{System.get_env("AWS_REGION")}.amazonaws.com/#{s3_key}"
   end
 end
